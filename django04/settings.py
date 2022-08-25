39ee8990bba23fc5e6689712357d523688fe7196
mohammadnpak <m.nozaripak@gmail.com>
2022-08-25 15:47:04 +0430

    extend some html with local base.html

--- django04/settings.py
+++ django04/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django04.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "accounts" / "static" ,
+    BASE_DIR / "static" ,
 ]
 
 # Default primary key field type
