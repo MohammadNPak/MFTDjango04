5f299aef3986d0150cd133e22045e3f5b0f9307b
mohammadnpak <m.nozaripak@gmail.com>
2022-09-15 18:06:13 +0430

    s06-c

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    bio = models.CharField(max_length=100)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to="profile_picture")
--- django04/settings.py
+++ django04/settings.py
@@ -123,6 +123,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static" ,
 ]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
