b59fb47d34adb77e92f6971b57ba006ed689059f
mohammadnpak <m.nozaripak@gmail.com>
2022-09-15 19:05:20 +0430

    s06-final

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -8,4 +9,20 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     picture = models.ImageField(upload_to="profile_picture")
     
-    
+
+class Education(models.Model):
+    school = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
+    field = models.CharField(max_length=100)
+    start = models.DateTimeField()
+    end = models.DateTimeField(null=True)
+    description = models.TextField()
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    start = models.DateTimeField()
+    end = models.DateTimeField(null=True)
+    description = models.TextField()
