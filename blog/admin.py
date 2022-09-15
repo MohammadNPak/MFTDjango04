46b72adc4337f21c7b77f5e5e36e013a0f3e451c
mohammadnpak <m.nozaripak@gmail.com>
2022-09-15 16:38:50 +0430

    s06-b

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Comment
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,16 @@ class Post(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    body = models.TextField()
+    is_validate =models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self) -> str:
+        return self.body[:50]
     
